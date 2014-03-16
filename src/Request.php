8909c802dd23cff7fb465ef9fcd6d6c37242dc17
Gajus Kuizinas <g.kuizinas@anuary.com>
2014-03-16 22:05:32 +0000

    Using 4 spaces instead of tabs for indenting.

--- src/Request.php
+++ src/Request.php
@@ -6,147 +6,147 @@ namespace Gajus\Strading;
  * @license https://github.com/gajus/strading/blob/master/LICENSE BSD 3-Clause
  */
 class Request {
-	private
-		$interface_url,
-		$headers = ['Content-Type: text/xml;charset=utf-8', 'Accept: text/xml'],
-		$dom,
-		$xpath,
-		$response;
+    private
+        $interface_url,
+        $headers = ['Content-Type: text/xml;charset=utf-8', 'Accept: text/xml'],
+        $dom,
+        $xpath,
+        $response;
 
-	/**
-	 * @param string $interface_url
-	 * @param string $site_reference
-	 * @param string $username
-	 * @param string $password
-	 * @param DOMDocument $dom
-	 */
-	public function __construct ($interface_url, $site_reference, $username, $password, \DOMDocument $dom) {
-		$this->interface_url = $interface_url;
-		$this->headers[] = 'Authorization: Basic ' . base64_encode($username . ':' . $password);
-		$this->dom = $dom;
-		$this->xpath = new \DOMXPath($dom);
+    /**
+     * @param string $interface_url
+     * @param string $site_reference
+     * @param string $username
+     * @param string $password
+     * @param DOMDocument $dom
+     */
+    public function __construct ($interface_url, $site_reference, $username, $password, \DOMDocument $dom) {
+        $this->interface_url = $interface_url;
+        $this->headers[] = 'Authorization: Basic ' . base64_encode($username . ':' . $password);
+        $this->dom = $dom;
+        $this->xpath = new \DOMXPath($dom);
 
-		$request_type = $this->xpath->query('/requestblock/request')->item(0)->getAttribute('type');
-		
-		if ($request_type === 'TRANSACTIONQUERY') {
-			$this->populate(['alias' => $username, 'request/filter/sitereference' => $site_reference], '/requestblock');
-		} else {
-			$this->populate(['alias' => $username, 'request/operation/sitereference' => $site_reference], '/requestblock');
-		}
-	}
-	
-	/**
-	 * Populate XML template with given data.
-	 * 
-	 * @param array $data ['node name' => 'text node value', 'node[attribute]' => 'attribute value', 'parent node' => ['child node' => 'text node value']]
-	 * @param string $namespace
-	 * @return void
-	 */
-	public function populate (array $data, $namespace = '') {
-		if ($this->response) {
-			throw new Exception\LogicException('Cannot populate data after request.');
-		}
+        $request_type = $this->xpath->query('/requestblock/request')->item(0)->getAttribute('type');
+        
+        if ($request_type === 'TRANSACTIONQUERY') {
+            $this->populate(['alias' => $username, 'request/filter/sitereference' => $site_reference], '/requestblock');
+        } else {
+            $this->populate(['alias' => $username, 'request/operation/sitereference' => $site_reference], '/requestblock');
+        }
+    }
+    
+    /**
+     * Populate XML template with given data.
+     * 
+     * @param array $data ['node name' => 'text node value', 'node[attribute]' => 'attribute value', 'parent node' => ['child node' => 'text node value']]
+     * @param string $namespace
+     * @return void
+     */
+    public function populate (array $data, $namespace = '') {
+        if ($this->response) {
+            throw new Exception\LogicException('Cannot populate data after request.');
+        }
 
-		foreach ($data as $k => $v) {
-			if (is_array($v)) {
-				$this->populate($v, $namespace . '/' . $k);
-			} else {
-				$attribute = null;
-				
-				if (($attribute_position = strpos($k, '[')) !== false) {
-					$attribute = substr($k, $attribute_position + 1, -1);
-					$k = substr($k, 0, $attribute_position);
-				}
-				
-				$element = $this->xpath->query($namespace . '/' . $k);
-				
-				if ($element->length === 0) {
-					throw new Exception\InvalidArgumentException($namespace . '/' . $k . ' path does not refer to an existing element.');
-				}
+        foreach ($data as $k => $v) {
+            if (is_array($v)) {
+                $this->populate($v, $namespace . '/' . $k);
+            } else {
+                $attribute = null;
+                
+                if (($attribute_position = strpos($k, '[')) !== false) {
+                    $attribute = substr($k, $attribute_position + 1, -1);
+                    $k = substr($k, 0, $attribute_position);
+                }
+                
+                $element = $this->xpath->query($namespace . '/' . $k);
+                
+                if ($element->length === 0) {
+                    throw new Exception\InvalidArgumentException($namespace . '/' . $k . ' path does not refer to an existing element.');
+                }
 
-				#else if ($element->length > 1) {
-				#	throw new \InvalidArgumentException($namespace . '/' . $k . ' path is referring to multiple elements.');
-				#}
-				
-				if ($attribute) {
-					$element->item(0)->setAttribute($attribute, $v);
-				} else {
-					$element->item(0)->nodeValue = '';
-					$element->item(0)->appendChild($this->dom->createTextNode($v));
-				}
-			}
-		}
-	}
+                #else if ($element->length > 1) {
+                #    throw new \InvalidArgumentException($namespace . '/' . $k . ' path is referring to multiple elements.');
+                #}
+                
+                if ($attribute) {
+                    $element->item(0)->setAttribute($attribute, $v);
+                } else {
+                    $element->item(0)->nodeValue = '';
+                    $element->item(0)->appendChild($this->dom->createTextNode($v));
+                }
+            }
+        }
+    }
 
-	/**
-	 * Request XML is stripped of empty tags without attributes.
-	 *
-	 * @return string
-	 */
-	public function getRequestXML () {
-		$dom = clone $this->dom;
-		$dom->preserveWhiteSpace = false;
-		$dom->formatOutput = true;
-		$xpath = new \DOMXPath($dom);
+    /**
+     * Request XML is stripped of empty tags without attributes.
+     *
+     * @return string
+     */
+    public function getRequestXML () {
+        $dom = clone $this->dom;
+        $dom->preserveWhiteSpace = false;
+        $dom->formatOutput = true;
+        $xpath = new \DOMXPath($dom);
 
-		// http://stackoverflow.com/a/21492078/368691
-		while (($node_list = $xpath->query('//*[not(*) and not(@*) and not(text()[normalize-space()])]')) && $node_list->length) {
-			foreach ($node_list as $node) {
-				$node->parentNode->removeChild($node);
-			}
-		}
+        // http://stackoverflow.com/a/21492078/368691
+        while (($node_list = $xpath->query('//*[not(*) and not(@*) and not(text()[normalize-space()])]')) && $node_list->length) {
+            foreach ($node_list as $node) {
+                $node->parentNode->removeChild($node);
+            }
+        }
 
-		$xml = tidy_repair_string($dom->saveXML(), array( 
-			'output-xml' => true, 
-			'input-xml' => true,
-			'indent' => true,
-			'wrap' => false
-		));
+        $xml = tidy_repair_string($dom->saveXML(), array( 
+            'output-xml' => true, 
+            'input-xml' => true,
+            'indent' => true,
+            'wrap' => false
+        ));
 
-		return $xml;
-	}
+        return $xml;
+    }
 
-	public function getRequestHeaders () {
-		return $this->headers;
-	}
-	
-	/**
-	 * Issue request.
-	 *
-	 * @return SimpleXMLElement
-	 */
-	public function request () {
-		if ($this->response) {
-			throw new Exception\LogicException('Cannot use the same Request instance for multiple requests.');
-		}
+    public function getRequestHeaders () {
+        return $this->headers;
+    }
+    
+    /**
+     * Issue request.
+     *
+     * @return SimpleXMLElement
+     */
+    public function request () {
+        if ($this->response) {
+            throw new Exception\LogicException('Cannot use the same Request instance for multiple requests.');
+        }
 
-		$this->response = $this->makeRequest();
+        $this->response = $this->makeRequest();
 
-		return new \SimpleXMLElement($this->response);
-	}
-	
-	/**
-	 * @return string
-	 */
-	private function makeRequest () {		
-		$ch = curl_init();
-		
-		$options = [
-			CURLOPT_URL => $this->interface_url,
-			CURLOPT_HEADER => false,
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_CONNECTTIMEOUT => 10,
-		    CURLOPT_TIMEOUT => 10,
-		    CURLOPT_SSL_VERIFYHOST => 2,
-		    CURLOPT_SSL_VERIFYPEER => true,
-		    CURLOPT_HTTPHEADER => $this->getRequestHeaders(),
-		    CURLOPT_POSTFIELDS => $this->getRequestXML()
-		];
-		
-		curl_setopt_array($ch, $options);
-		
-		$response = curl_exec($ch);
-		
-		return $response;
-	}
+        return new \SimpleXMLElement($this->response);
+    }
+    
+    /**
+     * @return string
+     */
+    private function makeRequest () {        
+        $ch = curl_init();
+        
+        $options = [
+            CURLOPT_URL => $this->interface_url,
+            CURLOPT_HEADER => false,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CONNECTTIMEOUT => 10,
+            CURLOPT_TIMEOUT => 10,
+            CURLOPT_SSL_VERIFYHOST => 2,
+            CURLOPT_SSL_VERIFYPEER => true,
+            CURLOPT_HTTPHEADER => $this->getRequestHeaders(),
+            CURLOPT_POSTFIELDS => $this->getRequestXML()
+        ];
+        
+        curl_setopt_array($ch, $options);
+        
+        $response = curl_exec($ch);
+        
+        return $response;
+    }
 }
--- src/Service.php
+++ src/Service.php
@@ -6,45 +6,45 @@ namespace Gajus\Strading;
  * @license https://github.com/gajus/strading/blob/master/LICENSE BSD 3-Clause
  */
 class Service {
-	private
-		$interface_url,
-		$site_reference,
-		$username,
-		$password;
-	
-	/**
-	 * Service is a Factory class carrying Secure Trading authentication parameters
-	 * and supplying Request with the request XML template.
-	 *
-	 * @param string $site_reference
-	 * @param string $username
-	 * @param string $password
-	 * @param string $interface_url
-	 */
-	public function __construct ($site_reference, $username, $password, $interface_url = 'https://webservices.securetrading.net:443/xml/') {
-		$this->interface_url = $interface_url;
-		$this->site_reference = $site_reference;
-		$this->username = $username;
-		$this->password = $password;
-	}
-	
-	/**
-	 * @param string $name Request template name, e.g. "card/order".
-	 * @return gajus\strading\Request
-	 */
-	public function request ($name) {
-		$template = __DIR__ . '/template/' . $name . '.xml';
-		
-		if (!file_exists($template)) {
-			throw new Exception\InvalidArgumentException('Request template does not exist.');
-		}
-		
-		$dom = new \DOMDocument();
-		$dom->load($template);
-		$dom->formatOutput = true;
+    private
+        $interface_url,
+        $site_reference,
+        $username,
+        $password;
+    
+    /**
+     * Service is a Factory class carrying Secure Trading authentication parameters
+     * and supplying Request with the request XML template.
+     *
+     * @param string $site_reference
+     * @param string $username
+     * @param string $password
+     * @param string $interface_url
+     */
+    public function __construct ($site_reference, $username, $password, $interface_url = 'https://webservices.securetrading.net:443/xml/') {
+        $this->interface_url = $interface_url;
+        $this->site_reference = $site_reference;
+        $this->username = $username;
+        $this->password = $password;
+    }
+    
+    /**
+     * @param string $name Request template name, e.g. "card/order".
+     * @return gajus\strading\Request
+     */
+    public function request ($name) {
+        $template = __DIR__ . '/template/' . $name . '.xml';
+        
+        if (!file_exists($template)) {
+            throw new Exception\InvalidArgumentException('Request template does not exist.');
+        }
+        
+        $dom = new \DOMDocument();
+        $dom->load($template);
+        $dom->formatOutput = true;
 
-		$request = new Request($this->interface_url, $this->site_reference, $this->username, $this->password, $dom);	
-		
-		return $request;
-	}
+        $request = new Request($this->interface_url, $this->site_reference, $this->username, $this->password, $dom);    
+        
+        return $request;
+    }
 }
