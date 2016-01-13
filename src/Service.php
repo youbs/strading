98d8825bcc844ddd698244714d4737a3ff86fcb8
stefanrk <stefan@graphicmail.net>
2016-01-13 15:13:14 +0200

    Xpay wrapper updates

--- src/Service.php
+++ src/Service.php
@@ -4,25 +4,27 @@ namespace Gajus\Strading;
 /**
  * Service is a factory for building the requests using a template and populating
  * the request with your credentials.
- * 
+ *
  * @link https://github.com/gajus/strading for the canonical source repository
  * @license https://github.com/gajus/strading/blob/master/LICENSE BSD 3-Clause
  */
-class Service {
+class Service
+{
     private
         $template_dir,
         $interface_url,
         $site_reference,
         $username,
         $password;
-    
+
     /**
      * @param string $site_reference The Merchant's Site Reference.
      * @param string $username
      * @param string $password
      * @param string $interface_url
      */
-    public function __construct ($site_reference, $username, $password, $interface_url = 'https://webservices.securetrading.net:443/xml/') {
+    public function __construct($site_reference, $username, $password, $interface_url = 'https://webservices.securetrading.net:443/xml/')
+    {
         $this->interface_url = $interface_url;
         $this->site_reference = $site_reference;
         $this->username = $username;
@@ -36,7 +38,7 @@ class Service {
      */
     public function setTemplateDirectory($dir)
     {
-        if ( ! is_dir($dir)) {
+        if (!is_dir($dir)) {
             throw new Exception\InvalidArgumentException("'{$dir}' is not a valid directory");
         }
 
@@ -52,22 +54,23 @@ class Service {
     {
         return $this->template_dir;
     }
-    
+
     /**
      * @param string $name Request template name, e.g. "card/order".
      * @return Gajus\Strading\Request
      */
-    public function request ($name) {
+    public function request($name)
+    {
         $template = $this->getTemplateDirectory() . '/' . $name . '.xml';
-        
+
         if (!file_exists($template)) {
             throw new Exception\InvalidArgumentException('Request template does not exist.');
         }
-        
+
         $xml = new \SimpleXMLElement(file_get_contents($template));
 
-        $request = new Request($this->interface_url, $this->site_reference, $this->username, $this->password, $xml);    
-        
+        $request = new Request($this->interface_url, $this->site_reference, $this->username, $this->password, $xml);
+
         return $request;
     }
 }
